f83d9203b5e4ac79008880f1a591c930647daefb
ppietropiccolo <pipiccolo2015@gmail.com>
2025-12-05 16:34:50 +0100

    zio pera

--- 2025/5/program_b.py
+++ 2025/5/program_b.py
@@ -1,30 +1,49 @@
-def included(new: tuple[int, int], old: tuple[int, int]):
-    new_start, new_end, old_start, old_end = new, old
+def compare(new_range: tuple[int, int], old_range: tuple[int, int]):
+    new_start, new_end = new_range
+    old_start, old_end = old_range
+    # what if old range has only one element?
+
     if new_end < old_start or new_start > old_end: # range completely outside
-        return "to add entirely"
-    if new_start < old_start and new_end > old_end: # starts before, ends inside 
-    if new_start > old_start and new_end < old_end: # completely included
-    if new_start > old_start and new_end > old_end: 
+        # print("separated")
+        return new_range
+    else:
+        result = min(new_start, old_start), max(new_end, old_end) # overlap
+        print(f"{new_range} overlaps with {old_range} so i will consider only {result}")
+        return result
 
 def solve():
     # i think it's better to only work with start and end indexes
 
     with open("input.txt") as f:
         text = f.readlines()
-    fresh: list[tuple[int, int]] = []
+    fresh: set[tuple[int, int]] = set()
 
     for line in text:
-        line = line[:-1]
-        if "-" not in line:
+        line = line[:-1] # remove last \n
+        if "-" not in line: # consider only lines with ranges
             break
-        new_start, new_end = line.split("-")
-        new_start, new_end = int(new_start), int(new_end)
+        new_range = tuple(int(i) for i in line.split("-")) # convert str -> tuple[int, int]
+        print(new_range)
+        to_remove = set()
+        for old_range in fresh: # loop for the ranges already in the set
+            current_range = compare(new_range, old_range) # get a temporary result of the comparison
+            if current_range != new_range: # if the comparison returned a new interval
+                to_remove.add(old_range) # remove the previous one
+                new_range = current_range # update the range to be compared with the next ones
+        
+        fresh.difference_update(to_remove) # remove all conflicting ranges
+        fresh.add(new_range) # add the total range, result of the comparisons
 
-        to_count = "yes"
-        for start, end in fresh:
-            if start <= new_start and new_end <= end:
-                to_count = "no"
-            elif new_start <= start and :
+    for rng1 in fresh:
+        for rng2 in fresh:
+            # if rng1 == rng2: continue
+            if compare(rng1, rng2) != rng1: # no range should overlap
+                print("error!!", rng1, rng2, compare(rng1, rng2))
+    # print(fresh)
+    result = sum(e-s+1 for s,e in fresh)
+    print(result)
+
+
 
 
 
